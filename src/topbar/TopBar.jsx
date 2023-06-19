59a94705d96d12bf0b05a5cc559eec569e45a2ab
Saurabh SIngh <saurabhsinghup@gmail.com>
2023-06-19 13:56:23 +0530

    first_commit

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,17 @@
+ import React from 'react'
+ import "./Topbar.css"
+ export default function TopBar() {
+   return (
+     <div className="top">
+       <div className="topLeft">
+       <i class="fa-brands fa-square-facebook"></i>
+       <i class="fa-brands fa-square-twitter"></i>
+       <i class="fa-brands fa-square-pinterest"></i>
+       <i class="fa-brands fa-square-instagram"></i>
+       </div>
+       <div className="topCenter">c</div>  
+       <div className="topRight">r</div> 
+     </div>
+   )
+ }
+ 
